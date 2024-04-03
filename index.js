021511231160488b6fa534bc5258ff2a438adf45
Derrick <omoriderrick@gmail.com>
2024-04-03 12:06:03 +0300

    New Feat: Array.prototype.find

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+  { year: '2015', result: 'W' },
+  { year: '2014', result: 'N/A' },
+  { year: '2013', result: 'L' },
+];
+function superbowlWin(data) {
+  for (let records of data) {
+    if (records.result === 'W') {
+      return `${records.year}`;
+    }
+  }
+}
+
+// record.prototype.find(superbowlWin);
+superbowlWin(record);
